54338dffc0c5acba68200cba2ce51a4a19775ebd
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-05 16:24:20 +0300

    Add global title section
    Добавлен заголовок с описанием к каждой секции

---
+++ src/components/home/SectionTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../sass/home/sectionTitle.sass';
+
+function SectionTitle({title}) {
+    return (
+        <div className='section-title'>
+            <h1 className="section-title__header">
+                {title}
+            </h1>
+            <p className="section-title__description">
+                Идейные соображения высшего порядка,<br/>
+                а также рамки и место обучения кадров
+            </p>
+        </div>
+    );
+}
+
+export default SectionTitle;
