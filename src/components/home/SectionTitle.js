b0c6b95b344f4c4c760edf48e5c3f7fdf09fec47
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-11 12:53:44 +0300

    Add feedback section
    Добавлена секция отзывов, стили к ней и адаптив

--- src/components/home/SectionTitle.js
+++ src/components/home/SectionTitle.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function SectionTitle({title}) {
+function SectionTitle({title, isWrap}) {
     return (
         <div className='section-title'>
             <h1 className="section-title__header">
                 {title}
             </h1>
             <p className="section-title__description">
-                Идейные соображения высшего порядка,<br/>
-                а также рамки и место обучения кадров
+                Идейные соображения высшего порядка,{isWrap ? '\na ' : ' a\n'}
+                также рамки и место обучения кадров
             </p>
         </div>
     );
