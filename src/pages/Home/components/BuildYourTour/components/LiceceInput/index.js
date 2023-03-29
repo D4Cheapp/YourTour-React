c43489b2f33d01399186b313a0b7dbf1b22bd43d
Scherbakov Ilya <hitrojopic@gmail.com>
2023-03-29 19:04:30 +0300

    Fix license agreement input
    Исправлено заполнение формы. Теперь чекбокс лицензионного соглашения обязателен.

--- src/pages/Home/components/BuildYourTour/components/LiceceInput/index.js
+++
@@ -1,1 +0,0 @@
-export {default as LicenceInput} from './LicenceInput'
--- src/pages/Home/components/BuildYourTour/components/index.js
+++ src/pages/Home/components/BuildYourTour/components/index.js
@@ -1,6 +1,6 @@
 import {FormInputFields} from './FormInputFields'
 import {FormButtons} from './FormButtons'
-import {LicenceInput} from "./LiceceInput";
+import {LicenceInput} from "./LiceseInput";
 import {AgeInput} from './AgeInput'
 
 export {FormInputFields, LicenceInput, FormButtons, AgeInput}
